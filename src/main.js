e27e8d83f4ada31085011714d5579d84545e43cd
t-adekunle <tayoadekunle97@gmail.com>
2024-03-22 12:32:17 +0000

    implemented capturing text selection

--- src/main.js
+++ src/main.js
@@ -2,9 +2,34 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-
+import { QuillEditor } from '@vueup/vue-quill'
 const app = createApp(App)
-
+const globalOptions =
+     {
+        debug: 'info',
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],
+            ['blockquote', 'code-block'],
+            ['link', 'image', 'video', 'formula'],
+            [{ header: 1 }, { header: 2 }],
+            [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
+            [{ script: 'sub' }, { script: 'super' }],
+            [{ indent: '-1' }, { indent: '+1' }],
+            [{ direction: 'rtl' }],
+            [{ size: ['small', false, 'large', 'huge'] }],
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            [{ color: [] }, { background: [] }],
+            [{ font: [] }],
+            [{ align: [] }],
+            ['clean'],
+          ],
+        },
+        placeholder: 'this is in main...',
+        readOnly: false,
+        theme: 'snow',
+      }
+QuillEditor.props.globalOptions.default = () => globalOptions
+app.component('QuillEditor', QuillEditor)
 app.use(router)
-
 app.mount('#app')
--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,188 +1,53 @@
 <template>
 	<div class="border-b">
-		<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-			<h1 class="text-3xl font-bold tracking-tight text-gray-900">
-				Edit Page
-			</h1>
-		</div>
+	  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+		<h1 class="text-3xl font-bold tracking-tight text-gray-900">
+		  Edit Page
+		</h1>
+	  </div>
 	</div>
-	<QuillEditor :options="options" id="editor" @selection-change="handleChange, range" />
-	<div id="editor"></div>
-</template>
-<script>
-import { QuillEditor, Quill } from '@vueup/vue-quill'
-import '@vueup/vue-quill/dist/vue-quill.snow.css'
-// import { getContents } from '@vueup/vue-quill'
-
-
-
-/*
-const quill = new Quill('#editor', {
-				debug: 'info',
-				modules: {
-					toolbar: [
-						['bold', 'italic', 'underline', 'strike'], // toggled buttons
-						['blockquote', 'code-block'],
-						['link', 'image', 'video', 'formula'],
-
-						[{ header: 1 }, { header: 2 }], // custom button values
-						[
-							{ list: 'ordered' },
-							{ list: 'bullet' },
-							{ list: 'check' },
-						],
-						[{ script: 'sub' }, { script: 'super' }], // superscript/subscript
-						[{ indent: '-1' }, { indent: '+1' }], // outdent/indent
-						[{ direction: 'rtl' }], // text direction
-
-						[{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
-						[{ header: [1, 2, 3, 4, 5, 6, false] }],
-
-						[{ color: [] }, { background: [] }], // dropdown with defaults from theme
-						[{ font: [] }],
-						[{ align: [] }],
-
-						['clean'],
-					],
-				},
-				placeholder: 'Start typing...',
-				readOnly: false,
-				theme: 'snow',
-
-			}
-);
-
-*/
-
-
-// quill.on('selection-change', (range, oldRange, source) => {
-//   if (range) {
-//     if (range.length == 0) {
-//       console.log('User cursor is on', range.index);
-//     } else {
-//       const text = quill.getText(range.index, range.length);
-//       console.log('User has highlighted', text);
-//     }
-//   } else {
-//     console.log('Cursor not in the editor');
-//   }
-// });
-
-// console.log(quill)
-
-export default {
+	<QuillEditor v-model="content" ref="quillEditor" :options="globalOptions" id="editor" @selection-change="handleChange" on />
+	<!-- <font-awesome-icon icon="fa-regular fa-comment" /> -->
+  </template>
+  <script>
+  import { QuillEditor } from '@vueup/vue-quill'
+  import '@vueup/vue-quill/dist/vue-quill.snow.css'
+  import Quill from 'quill'
+  export default {
 	components: {
-		 QuillEditor,
+	  QuillEditor,
 	},
-	data() {
-		return {
-			
-			options: {
-				debug: 'info',
-				modules: {
-					toolbar: [
-						['bold', 'italic', 'underline', 'strike'], // toggled buttons
-						['blockquote', 'code-block'],
-						['link', 'image', 'video', 'formula'],
-
-						[{ header: 1 }, { header: 2 }], // custom button values
-						[
-							{ list: 'ordered' },
-							{ list: 'bullet' },
-							{ list: 'check' },
-						],
-						[{ script: 'sub' }, { script: 'super' }], // superscript/subscript
-						[{ indent: '-1' }, { indent: '+1' }], // outdent/indent
-						[{ direction: 'rtl' }], // text direction
-
-						[{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
-						[{ header: [1, 2, 3, 4, 5, 6, false] }],
-
-						[{ color: [] }, { background: [] }], // dropdown with defaults from theme
-						[{ font: [] }],
-						[{ align: [] }],
-
-						['clean'],
-					],
-				},
-				placeholder: 'Start typing...',
-				readOnly: false,
-				theme: 'snow',
-
-			},
+	data: () => ({
+	  content: '',
+	  delta: undefined,
+	  selected: {}
+	}),
+	watch:{ 
+		content () {
+			this.delta = this.$refs.myQuillEditor.quill.getContents()
+			console.log(this.delta, '<<<<this.delta')
 		}
 	},
-
-	methods:{
-
-
-
-	//  handleChange : ((range, oldRange, source) => {
-	// 	if (range) {
-	// 		if(range.length == 0) {
-	// 			console.log('User cursor is on', range.index);
-	// 		}
-	// 		else{
-	// 			const text = this.getText(range.index, range.length)
-    //   			console.log('User has highlighted', text);
-	// 		}
-	// 	}
-	// 	else{
-	// 		console.log('Curser not in the editor')
-	// 	}
-	// })
-
-// 	if (range) {
-//     if (range.length == 0) {
-//       console.log('User cursor is on', range.index);
-//     } else {
-//       const text = this.getText(range.index, range.length);
-//       console.log('User has highlighted', text);
-//     }
-//    else {
-//     console.log('Cursor not in the editor');
-//   }
-// 	})
-// },
-
-	handleChange((event){
-		
-	console.log(event)
+	methods: {
+	  handleChange(range, oldRange, source) {
 	
-	const returnText = (range, oldRange, source) => {
-		console.log(range)
-
-  if (range) {
-    if (range.length == 0) {
-      console.log('User cursor is on', range.index);
-    } else {
-      const text = this.getText(range.index, range.length);
-      console.log('User has highlighted', text);
-    }
-  } else {
-    console.log('Cursor not in the editor');
+		if (range) {
+		  if (range.range["length"] === 0) {
+			console.log('User cursor is on', range.range["index"]);
+		  } else {
+			const text = this.$refs.quillEditor.getText(range.range["index"], range.range["length"]).trim();
+			console.log('User has highlighted', text);
+			this.selected.text = text
+		  }
+		} else {
+		  console.log('Cursor not in the editor');
+		}
+		console.log(this.selected)
+	  },
+	},
+	// mounted() {
+	//   this.QuillEditor = new QuillEditor('#editor', this.options);
+	//   this.QuillEditor.on('selection-change', this.handleChange);
+	// },
   }
-}
-returnText()
-})
-	},
-		
-	
-
-	computed: {
-		toolbar() {
-			return // remove formatting button
-		},
-	},
-}
-// const editor = new QuillEditor( '#quill-editor')
-
-// const textContent = editor.getSelection()
-
-const range = new Range(0,10)
-
-
-
-
-
-</script>
+  </script>
